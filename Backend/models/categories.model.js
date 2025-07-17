416f12cc58827600dfc1a846ecc3f19a12264c00
Controleur <18554067+controleur@users.noreply.github.com>
2025-07-17 09:24:18 +0300

    add id autoIncrement in models

--- Backend/models/categories.model.js
+++ Backend/models/categories.model.js
@@ -1,15 +1,19 @@
-
 module.exports = (sequelize, DataTypes) => {
-	const Categories = sequelize.define(
-		"categories",
-		{
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-			},
-		},
-		{timestamps: false}
-	)
-	return Categories
-}
+  const Categories = sequelize.define(
+    "categories",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    { timestamps: false }
+  );
+  return Categories;
+};
